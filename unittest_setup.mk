8caedb7ddce206c0ff14fcccec5dbae1e4b6fa91
cbleser <cr@tagion.org>
2021-07-21 12:52:19 +0300

    WIP: unittest updated

--- unittest_setup.mk
+++ unittest_setup.mk
@@ -1,5 +1,10 @@
-UNITTEST:=$(BIN)/uinttest_tvm
+UNITTEST:=$(BINDIR)/uinttest_tvm
+
+TESTINC:=$(REPOROOT)/lib_tvm/tests/basic/d/
+TESTDCFLAGS+=-I$(TESTINC) #REPOROOT)/lib_tvm/tests/basic/d/
+TESTDCFLAGS+=$(TESTINC)/src/native_impl.d
 
 TESTDCFLAGS+=-g
 TESTDCFLAGS+=-main
 TESTDCFLAGS+=-unittest
+TESTDCFLAGS+=-J$(REPOROOT)/lib_tvm/
